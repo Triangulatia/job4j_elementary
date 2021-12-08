f5197ce511cc151eb656fcb7ba08d55fc21ef9d0
master <triangulationtri@gmail.com>
2021-12-08 23:31:02 +0400

    6.9. Жадный алгоритм. Сдача в кофе машине [#34741 #237306] (вар. 1)

--- src/main/java/ru/job4j/array/Machine.java
+++ src/main/java/ru/job4j/array/Machine.java
@@ -7,9 +7,10 @@ public class Machine {
         int[] coins = {10, 5, 2, 1};
         int[] rsl = new int[100];
         int size = 0;
+        int leftMoney = money - price;
         for (int coin : coins) {
-            while (money - price >= coin) {
-                money -= coin;
+            while (leftMoney >= coin) {
+                leftMoney -= coin;
                 rsl[size] = coin;
                 size++;
             }
